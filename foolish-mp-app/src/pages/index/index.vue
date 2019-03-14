9875f8e01ed0fa3da9596bb7dfd52fa86fea5c33
johnnyKam <johnny_kam@163.com>
2019-03-14 16:16:27 +0800

    init:foolish-mp-app

---
+++ foolish-mp-app/src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    小程序
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
